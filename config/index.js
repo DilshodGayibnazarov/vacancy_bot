f7061b3de67b638f750c93e0c3e4339aca4a5eb7
dilshodgayibnazarov <just_dilshod@mail.ru>
2023-11-14 15:20:13 +0500

    changes added

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
     environment: getConf("NODE_ENV", ""),
-    telegramId: getConf("TELEGRAM_ID", "-1001942967380"),
+    telegramId: getConf("TELEGRAM_ID", "write your telegram id"),
     telegramToken: getConf("TG_BOT_TOKEN", "Write your bot Token"),
     redisUrl: getConf("REDIS_URL", ""),
     webhookUrl : getConf("WEBHOOK_URL", ""),
